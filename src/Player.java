ffbe5dfb6dfca9f35fbecbb4e37a307e9cc0390d
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-05-28 12:58:14 +0200

    Added docs

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,22 @@
 import java.awt.Color;
 import java.util.ArrayList;
 
+/*
+ * 
+ * @author Felix Lehner
+ */
 public class Player {
-	
-	private String name;
-	private Color color;
+	/** The name that gets displayed. */
+	final private String name;
+	/** The player's color */
+	final private Color color;
+	/** The player's hand. */
 	private ArrayList<Card> cards;
+	/**  */
 	private ArrayList<Game.Area> areas;
-	
 	public Player(String name, Color color) {
 		this.name = name;
 		this.color = color;
-		
 	}
 	
 }
