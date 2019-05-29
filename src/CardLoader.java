5d413f0926f304a5ed1f947dcd0b6fcc530472e5
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-05-29 11:16:16 +0200

    Create CardLoader.java

---
+++ src/CardLoader.java
@@ -0,0 +1,17 @@
+import java.awt.Image;
+
+/**
+  *
+  *
+  *
+  *
+  * @author Felix Lehner
+  */
+class CardLoader {
+  /**
+    *
+    * @author Felix Lehner
+    */
+  public CountryCard[] loadFrom (String path) {
+  }
+}
