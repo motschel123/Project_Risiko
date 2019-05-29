22af2c519849b345622699d74841ebf0dff64691
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-05-30 01:38:51 +0200

    Update CardLoader.java
    The loader should now load a file and separate it's lines but the card-loading process still needs to be implemented.

--- src/CardLoader.java
+++ src/CardLoader.java
@@ -1,17 +1,52 @@
 import java.awt.Image;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
   *
-  *
+  * A loader for cards.
   *
   *
   * @author Felix Lehner
   */
 class CardLoader {
   /**
+    * Loads the cards and their attributes from a specified CSV file.
     *
     * @author Felix Lehner
     */
   public CountryCard[] loadFrom (String path) {
+    try {
+      FileReader reader = new FileReader(path);
+      char character = (char) reader.read();
+      ArrayList<String> lines = new ArrayList<String>();
+      lines.add("");
+      while (character != -1) {
+         if (character != "\n) {
+           String temp = lines.get(lines.size() -1);
+           temp += character
+           lines.set(lines.size() - 1, temp);
+         } else {
+           lines.add("");
+         }
+         character = (char) reader.read();
+      }
+      reader.close();
+      return parse(lines);
+    }
+    //TODO specify error messages
+    catch (FileNotFoundException e) {
+      System.err.println("Failed to laod the file " + path);
+      return null;
+    }
+    catch (IOException io) {
+     System.err.println("Failed to load the file " + path);
+     return null;
+    }
+  }
+             
+  private CountryCard[] parse (ArrayList<String> lines) {
+    CountryCard[] res = new CountryCard[lines.size()];
+    //TODO load the cards with the attributes contained in each line
   }
 }
