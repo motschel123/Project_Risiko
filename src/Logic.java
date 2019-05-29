cdfddf9830cc3c7530376c2b25f597749966022f
Motschel <marcel.schoeckel@gmail.com>
2019-05-29 11:06:31 +0200

    Added shuffle method

--- src/Logic.java
+++ src/Logic.java
@@ -1,5 +1,19 @@
 
 public class Logic {
 	
+	public static shuffle(ArrayList<Player> players, ArrayList<Card> cards){
+		players = shufflePlayers(players);
+		cards = shufflePlayers(cards);
+	}
+	
+	public static ArrayList<Card> shuffleCards(ArrayList<Card> cards){
+		Collection.shuffle(cards);
+		return cards;
+	}
+		
+	public static ArrayList<Player> shufflePlayers(ArrayList<Player> players){
+		Collection.shuffle(players);
+		return players;
+	}
 	
 }
