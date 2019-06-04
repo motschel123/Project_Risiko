503a33eff034e95268619f034cfd4e6763c9e8cc
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-06-04 12:55:33 +0200

    Update CountryCard.java
    Replaced Area with Country

--- src/CountryCard.java
+++ src/CountryCard.java
@@ -7,7 +7,7 @@ import java.awt.Image;
   * @author Felix Lehner
   */
 class CountryCard extends Card {
-  public CountryCard (int stars, Game.Area area, Image icon) {
+  public CountryCard (int stars, Country area, Image icon) {
     this.Stars = stars;
     this.area = area;
     this.icon = icon;
