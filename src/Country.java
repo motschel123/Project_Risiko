72b13d940c8cd9bdfff10141460c568dd35eb0e1
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-06-04 12:33:21 +0200

    Create Country.java

---
+++ src/Country.java
@@ -0,0 +1,17 @@
+/**
+ *
+ *
+ *
+ * @since 04.06.19
+ * @author Felix Lehner
+ */
+class Country {
+  /**All countries with a direct border (i.e. counties that can be attacked)*/
+  private Country[] borders;
+  /**The player that is currently in power of this country*/
+  private Player owner;
+  /**name of the country*/
+  private String name;
+  /**The amount of troop units*/
+  private int troopPower;
+}
