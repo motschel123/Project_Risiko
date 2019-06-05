d4e222dc8f8703ee289806145d5ef21fcf547c8d
Motschel <marcel.schoeckel@gmail.com>
2019-06-05 11:11:04 +0200

    Update Country.java

--- src/Country.java
+++ src/Country.java
@@ -6,12 +6,44 @@
  * @author Felix Lehner
  */
 class Country {
-  /**All countries with a direct border (i.e. counties that can be attacked)*/
-  private String[] borders;
-  /**The player that is currently in power of this country*/
-  private Player owner;
-  /**name of the country*/
-  private String name;
-  /**The amount of troop units*/
-  private int unitPower;
+	/**All countries with a direct border (i.e. counties that can be attacked)*/
+	private String[] borders;
+	/**The player that is currently in power of this country*/
+	private Player owner;
+	/**name of the country*/
+	private String name;
+	/**The amount of troop units*/
+	private int unitPower;
+  
+	/**
+	 * Constructor
+	 *
+	 * @author Marcel Schöckel
+	 */
+	public Country(String name, String[] borders){
+  		this.name = name;
+		this.borders = bordes;
+   	}
+	
+	/**
+	 * Getter methods
+	 *
+	 * @author Marcel Schöckel
+	 */
+	public String getName(){
+		return name;
+	}
+	
+	public Player getOwner(){
+		return owner;
+	}
+	
+	public int getUnitPower(){
+		return unitPower;
+	}
+	
+	public String[] getBorders(){
+		return borders;
+	}
+	
 }
