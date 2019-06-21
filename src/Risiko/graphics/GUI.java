e8c45e3fc7bbe9b10015d92b96b0c65fe23d7360
Marcel S. <marcel.schoeckel@gmail.com>
2019-06-21 14:38:58 +0200

    Added exit button

--- src/Risiko/graphics/GUI.java
+++ src/Risiko/graphics/GUI.java
@@ -1,31 +1,27 @@
 package Risiko.graphics;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import javax.swing.JLabel;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.GroupLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
-import javax.swing.GroupLayout.Alignment;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
 
-import Risiko.Game;
-
-import com.jgoodies.forms.layout.FormSpecs;
-import java.awt.CardLayout;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class GUI {
 
 	private JFrame frame;
+	
+	private JMenuBar menuBar_1;
+	private JMenu jmGame;
+	private JMenuItem jmItemExit;
+	
+	private JLayeredPane layeredPane;
+	private JLabel bgLabel;
+	private JPanel panel;
 	
 	/**
 	 * Create the application.
@@ -40,20 +36,41 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// init the basic frame holding everything
 		frame = new JFrame();
-		//frame.setUndecorated(true);  // fullscreen
+		frame.setUndecorated(true);  // fullscreen
+		frame.setResizable(false);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLayeredPane layeredPane = new JLayeredPane();
+		// init the menu bar on the top to display control options
+		menuBar_1 = new JMenuBar();
+		frame.setJMenuBar(menuBar_1);
+		
+		jmGame = new JMenu("Game");
+		menuBar_1.add(jmGame);
+		
+		jmItemExit = new JMenuItem("Exit");
+		jmItemExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				System.exit(0);
+			}
+		});
+		
+		jmGame.add(jmItemExit);
+		
+		// init the layered pane to hold multiple other components ordered
+		layeredPane = new JLayeredPane();
 		frame.getContentPane().add(layeredPane, BorderLayout.CENTER);
 		
-		JLabel bgLabel = new JLabel();
+		// init the background
+		bgLabel = new JLabel();
 		bgLabel = new JLabel(new ImageIcon(getClass().getResource("map.png")));
 		layeredPane.setLayout(new BorderLayout(0, 0));
 		layeredPane.add(bgLabel);
 		
-		JPanel panel = new JPanel();
+		
+		panel = new JPanel();
 		layeredPane.add(panel, BorderLayout.NORTH);
 	}
 
