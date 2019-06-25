fbfcd58405b75b6e7b4d246aca4aa48479b073df
SlaynAndKorpil <50499590+SlaynAndKorpil@users.noreply.github.com>
2019-06-25 12:57:41 +0200

    Create GameStatus.java

---
+++ src/Risiko/GameStatus.java
@@ -0,0 +1,14 @@
+package Risiko;
+
+/**
+  * Contains all possible states the game can be in.
+  *
+  * @author Felix Lehner
+  *
+  */
+class GameStatus {
+  public static int errorStatus = -1;
+  public static int waiting = 0;
+  public static int attacking = 1;
+  //TODO add all other states
+}
